36468601bf82f77226b288e9f30168f2862f32ce
warrior-coder <gudkou2003@gmail.com>
2022-11-03 20:33:06 +0300

    Solve ex 14.

--- sql_ex_3.sql
+++ sql_ex_3.sql
@@ -1,1 +1,5 @@
 -- 14. Find the ships in the Ships table that have at least 10 guns, get the class, name, and country.
+SELECT ships.class, ships.name, classes.country
+FROM ships
+JOIN classes ON ships.class = classes.class
+WHERE classes.numGuns >= 10;
